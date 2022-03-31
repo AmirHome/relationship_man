70ca5ce398558cba58b1a3fed1f3486b6c4ab938
amirhome <amir.email@yahoo.com>
2022-03-31 16:14:50 +0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:pusher_channels/pusher_channels.dart';
 
-void main() {
+main() async{
+  final pusher = Pusher(key: 'ecd279b8357d739168ee');
+  await pusher.connect();
+  final channel = pusher.subscribe('my-channel');
+  channel.bind('my-event', (event) {
+    print('WOW event: $event');
+  });
+
   runApp(const MyApp());
 }
 
